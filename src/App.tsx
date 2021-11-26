de5a5bc1d9181cf3d57430dabd5aea366f5b77b5
Mauro Porras <167700+mauroporras@users.noreply.github.com>
2021-11-26 12:17:07 -0500

    feat: tree view component data binding (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { Scene } from '@zeainc/zea-engine'
+import { useEffect, useState } from 'react'
 import { ReflexContainer, ReflexSplitter, ReflexElement } from 'react-reflex'
 
 import { Viewport3D } from './Viewport3D'
@@ -7,25 +8,29 @@ import { ZeaTreeViewWrapper } from './ZeaTreeViewWrapper'
 import 'react-reflex/styles.css'
 import './App.css'
 
-class App extends React.Component {
-  render() {
-    return (
-      <ReflexContainer orientation="horizontal">
-        <ReflexElement size={50}>Header</ReflexElement>
-        <ReflexElement>
-          <ReflexContainer orientation="vertical">
-            <ReflexElement size={200}>
-              <ZeaTreeViewWrapper></ZeaTreeViewWrapper>
-            </ReflexElement>
-            <ReflexSplitter />
-            <ReflexElement>
-              <Viewport3D></Viewport3D>
-            </ReflexElement>
-          </ReflexContainer>
-        </ReflexElement>
-      </ReflexContainer>
-    )
-  }
+const App = () => {
+  const [scene] = useState<Scene>(new Scene())
+
+  useEffect(() => {
+    console.log('hi')
+  })
+
+  return (
+    <ReflexContainer orientation="horizontal">
+      <ReflexElement size={50}>Header</ReflexElement>
+      <ReflexElement>
+        <ReflexContainer orientation="vertical">
+          <ReflexElement size={200}>
+            <ZeaTreeViewWrapper scene={scene} />
+          </ReflexElement>
+          <ReflexSplitter />
+          <ReflexElement>
+            <Viewport3D scene={scene} />
+          </ReflexElement>
+        </ReflexContainer>
+      </ReflexElement>
+    </ReflexContainer>
+  )
 }
 
 export default App
