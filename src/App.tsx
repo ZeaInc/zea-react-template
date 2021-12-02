67ab5497f6b09af19f0640cc5a2de0bc7805578f
Seiji-zea Smith <seiji.smith@zea.live>
2021-12-02 14:27:15 -0800

    wip: popup for file drop

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,50 @@ import 'react-reflex/styles.css'
 import './App.css'
 import Dropzone from 'react-dropzone'
 
+import Popup from 'reactjs-popup'
+import 'reactjs-popup/dist/index.css'
+
 const App = () => {
   const [scene] = useState<Scene>(new Scene())
   const [file, setFile] = useState<any>(null)
-
+  const [open, setOpen] = useState<boolean>(true)
   useEffect(() => {
     console.log('hi')
   })
 
   return (
     <ReflexContainer orientation="horizontal">
-      <ReflexElement size={50}>Header</ReflexElement>
+      <ReflexElement size={50}>
+        <Popup open={open} modal nested>
+          {(close: any) => (
+            <div className="modal">
+              <button className="close" onClick={close}>
+                &times;
+              </button>
+
+              <Dropzone
+                onDrop={(acceptedFiles) => {
+                  setFile(acceptedFiles)
+                  setOpen(false)
+                }}
+              >
+                {({ getRootProps, getInputProps }) => (
+                  <section>
+                    <div {...getRootProps()}>
+                      <input {...getInputProps()} />
+
+                      <p>
+                        Drag 'n' drop some files here, or click to select files
+                      </p>
+                    </div>
+                  </section>
+                )}
+              </Dropzone>
+            </div>
+          )}
+        </Popup>
+        Header
+      </ReflexElement>
       <ReflexElement>
         <ReflexContainer orientation="vertical">
           <ReflexElement size={200}>
@@ -27,18 +60,6 @@ const App = () => {
           </ReflexElement>
           <ReflexSplitter />
           <ReflexElement>
-            <Dropzone onDrop={(acceptedFiles) => setFile(acceptedFiles)}>
-              {({ getRootProps, getInputProps }) => (
-                <section>
-                  <div {...getRootProps()}>
-                    <input {...getInputProps()} />
-                    <p>
-                      Drag 'n' drop some files here, or click to select files
-                    </p>
-                  </div>
-                </section>
-              )}
-            </Dropzone>
             <Viewport3D scene={scene} file={file} />
           </ReflexElement>
         </ReflexContainer>
